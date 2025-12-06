09e43d1b5cad635a8235744c8005e6cb2a061107
Ajaj Mahmud Aquil <ajajmahmudaquil0@gmail.com>
2025-12-06 20:44:28 +0600

    add file

---
+++ main.py
@@ -0,0 +1,18 @@
+#Colorful Rainbow Circle using Python
+#Made with ❤ by Aquil
+
+import turtle
+
+t = turtle.Turtle()
+t.speed(10)
+
+colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+
+turtle.bgcolor("black")
+
+for i in range(36):
+    t.color(colors[i % 7])
+    t.circle(100)
+    t.right(100)
+
+turtle.done()
